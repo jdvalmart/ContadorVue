3ad18bb727c54cfdc8dc2c6a0f8ef69525d0b092
jdvalmart <juanvalencia9411@outlook.com>
2024-04-04 23:46:31 -0500

    paginacion del blog

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <script setup>
-import BlogPost from "./components/BlogPost.vue"
 import { ref } from "vue";
 
-const posts = ref([
+import BlogPost from "./components/BlogPost.vue";
+import PaginatePost from "./components/PaginatePost.vue";
 
-]);
+
+const posts = ref([]);
+const postXpage = 10;
+const inicio = ref(0);
+const fin = ref(postXpage);
+
 const favorito = ref("")
 
 const cambiarFavorito = (title) => {
   favorito.value = title;
 };
+
+const next = () => {
+  inicio.value += + postXpage;
+  fin.value += + postXpage;
+}
+const previo = () => {
+  inicio.value += - postXpage;
+  fin.value += - postXpage;
+}
 
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(res => res.json() )
@@ -23,13 +37,20 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     <h1>APP</h1>
     <h2>Mi post favorito: {{ favorito }}</h2>
 
+      
+    <PaginatePost class="mb-2"
+      @next="next"
+      @previo="previo"
+    />
+    
     <BlogPost
-    v-for="post in posts"
+    v-for="post in posts.slice(inicio, fin)"
     :key="post.id"
     :title="post.title"
     :id="post.id"
     :body="post.body"
     @cambiarFavoritoNombre="cambiarFavorito"
+    class="mb-2"
     ></BlogPost>  
    
   </div>
---
+++ src/components/PaginatePost.vue
@@ -0,0 +1,9 @@
+<script setup>
+const emit = defineEmits(['next', 'previo'])
+</script>
+<template>
+  <div class="btn-group" role="group" aria-label="Basic example">
+    <button @click="emit('previo')" type="button" class="btn btn-outline-primary">Previus</button>
+    <button @click="emit('next')"  type="button" class="btn btn-outline-primary">Next</button>
+  </div>
+</template>
